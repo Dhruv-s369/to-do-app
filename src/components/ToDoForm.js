50ebe7758f8aa7fc8d61cc06b712bfcb089f9c68
Dhruv <dhruvsartanpara19@gmai.com>
2023-03-19 17:10:49 +0530

    just divided the logic and imported the todoform.

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -2,30 +2,9 @@ import React, { useState, useEffect } from "react";
 import ToDoApp from "./ToDoApp";
 import "../components/Card.css";
 
-const getItemsFromLS = () => {
-  const list = JSON.parse(localStorage.getItem("lists"));
-  if (list) {
-    return JSON.parse(localStorage.getItem("lists"));
-  } else {
-    return [];
-  }
-};
-
 function ToDoForm() {
   const [input, setInput] = useState("");
-  const [items, setItems] = useState(getItemsFromLS);
-
-  useEffect(() => {
-    localStorage.setItem("lists", JSON.stringify(items));
-  }, [items]);
-
-  const handleDelete = (id) => {
-    const UpdatedItem = items.filter((element, ind) => {
-      return ind != id; // If the index number(ind) in filter method is not matching the id from the delete (id - came from the map function) then return only those items and leave the rest.
-    });
-    setItems(UpdatedItem);
-    localStorage.removeItem("lists");
-  };
+  const [items, setItems] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,11 +13,6 @@ function ToDoForm() {
       setItems([...items, input]);
       setInput("");
     }
-
-    // props.onSubmit({
-    //   text: input,
-    // });
-    // setInput("");
   };
 
   return (
@@ -59,9 +33,6 @@ function ToDoForm() {
                 <h2 className="items">
                   <span className="text">{item}</span>
                 </h2>
-                <button className="del-btn" onClick={() => handleDelete(idx)}>
-                  Delete
-                </button>
               </div>
             );
           })}
